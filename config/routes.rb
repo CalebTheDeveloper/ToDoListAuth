96e4ed7c9cca1a75dc20b2f84a9c3ccd8103fdf9
CalebTheDeveloper <150725721+CalebTheDeveloper@users.noreply.github.com>
2024-01-15 15:13:56 -0700

    This app passes all tests.

---
+++ config/routes.rb
@@ -0,0 +1,63 @@
+Rails.application.routes.draw do
+  get 'sessions/users'
+
+  root 'homepage#index'
+
+  # Add routes below this line
+  # ...
+
+  # USERS
+  post '/users' => 'users#create'
+
+  # ...
+  # ...
+
+  # USERS
+  post '/users' => 'users#create'
+
+  # SESSIONS
+  post '/sessions' => 'sessions#create'
+
+  # ...
+  # ...
+
+  # USERS
+  post '/users' => 'users#create'
+
+  # SESSIONS
+  # ...
+  get '/authenticated' => 'sessions#authenticated'
+
+  # ...
+  # ...
+
+  # USERS
+  post '/users' => 'users#create'
+
+  # SESSIONS
+  # ...
+  delete '/sessions' => 'sessions#destroy'
+
+  # ...
+
+  # TASKS
+  get    '/tasks'                    => 'tasks#index'
+  post   '/tasks'                    => 'tasks#create'
+  delete '/tasks/:id'                => 'tasks#destroy'
+  put    '/tasks/:id/mark_complete'  => 'tasks#mark_complete'
+  put    '/tasks/:id/mark_active'    => 'tasks#mark_active'
+  get    '/my_tasks'                 => 'tasks#index_by_current_user'
+
+  # USERS
+  post   '/users'                    => 'users#create'
+
+  # SESSIONS
+  post   '/sessions'                 => 'sessions#create'
+  get    '/authenticated'            => 'sessions#authenticated'
+  delete '/sessions'                 => 'sessions#destroy'
+
+  # Add routes below above line
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  get '*path' => 'homepage#index'
+end
---
+++ db/migrate/20240115203133_create_session.rb
@@ -0,0 +1,8 @@
+class CreateSession < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sessions do |t|
+
+      t.timestamps
+    end
+  end
+end
